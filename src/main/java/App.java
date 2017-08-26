13f5d81f7e4fb04fa011503b0fce67af682da7f0
EMichira <emmanuelmichira@gmail.com>
2017-08-26 15:42:03 +0300

    Added logic to file to hold answers in an arraylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class App {
-  public static void main(String[] args) {
+
+    public static void main(String[] args){
+      Console console = System.console();
+
+      List<String> myAnswers = Arrays.asList("ford", "rangerover", "mercedesbenz", "bmw", "jaguar", "audi");
+      Hangman myGame = new Hangman(myAnswers);
+
+      System.out.println("Welcome to the game Hangman:");
+      System.out.println("Kindly guess our mystrey word.");
+      System.out.println("Hint: cars");
 
 
-  }
+    }
 }
