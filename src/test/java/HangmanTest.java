c755b867c767377e2c1ae19739cb2fc4bc6dbf64
EMichira <emmanuelmichira@gmail.com>
2017-08-27 12:38:00 +0300

    Added tests to check if implemented behaviour passes

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -6,9 +6,30 @@ import static org.junit.Assert.*;
 public class HangmanTest {
   @Test
   public void hangman_atLeastReturnsAString_String(){
-      Hangman testGame = new Hangman();
-      List<String> myAnswer = Arrays.asList("ford");
-      String guessedLetter = testGame.guessedLetters(myWord);
-      assertEquals(true, computerChoice!=null);
+    Hangman testGame = new Hangman();
+    List<String> myAnswer = Arrays.asList("ford");
+    String guessedLetter = testGame.guessedLetters(myWord);
+    assertEquals(true, computerChoice!=null);
   }
+  @Test
+  public void hangman_returnsARandomWordFromStringAnswerList_String() {
+    Hangman game = new Hangman();
+    List<String> myAnswers = Arrays.asList("ford", "rangerover", "mercedesbenz", "bmw", "jaguar", "audi");
+    String hangmanChoice = game.hangman(myAnswers);
+    assertEquals(true,myAnswers.contains(hangmanChoice));
+  }
+  @Test
+  public void getGuessedLetter_returnsStringDashEqualToStringLength_String();
+    Hangman game = new Hangman();
+    game.mAnswer = "ford";
+    String choosedAnswer = game.getGuessedLetter("");
+    assertEquals("____", choosedAnswer);
+  @Test
+  public void getGuessedLetter_returnsStringWithGuessedLetter_String(){
+    Hangman game = new Hangman();
+    game.mAnswer = "ford";
+    String choosedAnswer = game.getGuessedLetter("f");
+    assertEquals("f___",choosedAnswer);
+  }
+
 }
